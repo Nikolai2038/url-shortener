1bd3f996542c4d639befb0121a0c379e4ca9ddc4
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-03-24 20:41:58 +0300

    Return proper JSON

--- src/main/java/url_shortener/shortener/controllers/ShortenController.java
+++ src/main/java/url_shortener/shortener/controllers/ShortenController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import url_shortener.shortener.models.ShortenRequest;
+import url_shortener.shortener.models.ShortenResponse;
 import url_shortener.shortener.services.ShortenerService;
 
 @RestController
@@ -14,7 +15,7 @@ public class ShortenController {
   private ShortenerService shortenService;
 
   @PostMapping("/shortUrl")
-  public String shorten(@RequestBody ShortenRequest request) {
-    return shortenService.shorten(request.getLongUrl());
+  public ShortenResponse shorten(@RequestBody ShortenRequest request) {
+    return new ShortenResponse(shortenService.shorten(request.getLongUrl()));
   }
 }
